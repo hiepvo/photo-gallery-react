b22e20b8e7c52fa576483bddcae0afafbfd92a10
hiep.vo <vanhiep1086@gmail.com>
2017-10-11 16:14:26 -0400

    lazy load

--- app/components/Photos.js
+++ app/components/Photos.js
@@ -1,11 +1,26 @@
 let React     = require('react');
 let Link      = require('react-router-dom').Link;
 let PropTypes = require('prop-types');
-import LazyLoad from 'react-lazyload';
+
+import Lazyload from 'react-lazyload';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+function imagesLoaded(parentNode){
+  const imgElements = parentNode.querySelectorAll('img');
+  for(const img of imgElements){
+    if(!img.complete){
+      return false;
+    }
+  }
+  return true;
+}
 
 class Photos extends React.Component {
   constructor(props){
     super(props);
+    this.state       = {
+      loading: true
+    }
     this.handleClick = this.handleClick.bind(this);
   }
 
@@ -14,22 +29,37 @@ class Photos extends React.Component {
     onClick(event, {photo, index});
   }
 
+  handleImageChange(){
+    const galleryElement = this.refs.thumbnail;
+    this.setState({
+      loading: !imagesLoaded(galleryElement),
+    });
+  }
+
+  renderSpinner(){
+    if(!this.state.loading){
+      return null;
+    }
+    return (
+        <span className = "spinner"/>
+    );
+  }
+
   render(){
     const {photo, onClick} = this.props;
     return (
-        <div className = "photo-thumbnail">
-          <LazyLoad throttle = {200} height={200}>
-              <img
-                  src = {photo.src}
-                  onClick = {onClick ? this.handleClick : null}
-              />
-          </LazyLoad>
-          <span className = "info-overlay">
+        <Lazyload throttle = {200} height = {300}>
+
+          <div className = "photo-thumbnail" ref = "thumbnail">
+            <img
+                src = {photo.src}
+                onClick = {onClick ? this.handleClick : null}
+            />
+            <span className = "info-overlay">
             <Link to = "/galleries" className = "info-title">{photo.name}</Link>
           </span>
-          <div className="clearFix" ></div>
-        </div>
-
+          </div>
+        </Lazyload>
     )
   }
 }
